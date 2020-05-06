75fa0058d3815188040fa820d5123fe614808fab
YoshThePinetree <p349532y@mail.kyutech.jp>
2020-05-06 11:33:01 +0900

    Initial Commit

---
+++ ICA1/src/defo/Main.java
@@ -0,0 +1,9 @@
+package defo;
+
+public class Main {
+	public static void main (String arg[]) {
+		System.out.println("Imperialist Competitive Algorithm");
+		ICA ica = new ICA();
+		ica.ImperialistCompetitiveAlgorithm();
+	}
+}
